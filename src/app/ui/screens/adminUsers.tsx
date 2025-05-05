bba1b60d76d4e7e05779f6d87e51e351ed24bcc6
Carlos Lukeni <carloslukeni110@gmail.com>
2025-05-05 23:04:55 +0100

    Layout para a área de management da fila tá criado

--- src/app/ui/screens/adminUsers.tsx
+++ src/app/ui/screens/adminUsers.tsx
@@ -1,4 +1,12 @@
-import { Box, Heading, Button, Stack, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Button,
+  Stack,
+  Input,
+  Text,
+  Alert,
+} from "@chakra-ui/react";
 import SidebarMenu from "../components/SidebarMenu";
 import { useState } from "react";
 import DialogModal from "../components/DialogModal";
@@ -7,122 +15,151 @@ import { database } from "../../database/firebase";
 import { Field } from "../../../components/ui/field";
 
 export default function AdminUsers() {
-    const [isOpen, setIsOpen] = useState(false);
-    const [nome, setNome] = useState("");
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [loading, setLoading] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async () => {
-        if (!nome || !email || !senha) {
-            alert("Preencha todos os campos!");
-            return;
-        }
+  const handleSubmit = async () => {
+    if (!nome || !email || !senha) {
+      alert("Preencha todos os campos!");
+      return;
+    }
 
-        try {
-            // Adicionando usuário ao Firestore
-            await addDoc(collection(database, "atendentes"), {
-                nome,
-                email,
-                senha,
-                createdAt: new Date(),
-                lastLogin: new Date(),
-                updatedAt: new Date(),
-            });
+    setLoading(true);
 
-            alert("Usuário cadastrado com sucesso!");
-            setIsOpen(false);
-            setNome("");
-            setEmail("");
-            setSenha("");
-            
-        } catch (error) {
-            console.error("Erro ao cadastrar:", error);
-            alert("Erro ao cadastrar usuário!");
-        } finally{
-            setLoading(false)
-        }
-    };
+    try {
+      // Adicionando usuário ao Firestore
+      await addDoc(collection(database, "atendentes"), {
+        nome,
+        email,
+        senha,
+        createdAt: new Date(),
+        lastLogin: new Date(),
+        updatedAt: new Date(),
+      });
 
-    return (
-        <Box p="5">
-            <Box display="grid" gridTemplateColumns="300px 1fr" gap="10">
-                <SidebarMenu />
+      alert("Usuário cadastrado com sucesso!");
+      setOpen(false);
+      setNome("");
+      setEmail("");
+      setSenha("");
+    } catch (error) {
+      console.error("Erro ao cadastrar:", error);
+      alert("Erro ao cadastrar usuário!");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-                <Box bg="white" h="90vh" rounded="md" color="#00476f">
-                    <Heading size="3xl" fontWeight="bold" mb={6} textAlign="center" color="#00476f">
-                        Registre novo usuário
-                    </Heading>
+  return (
+    <Box p="5">
+      <Box display="grid" gridTemplateColumns="300px 1fr" gap="10">
+        <SidebarMenu />
 
-                    <Stack gap="10"></Stack>
+        <Box bg="white" h="90vh" rounded="md" color="#00476f">
+          <Heading
+            size="3xl"
+            fontWeight="bold"
+            mb={6}
+            textAlign="center"
+            color="#00476f"
+          >
+            Registre novo usuário
+          </Heading>
 
-                    <Button
-                        bg="#00476f"
-                        fontSize="3xl"
-                        w="70px"
-                        h="70px"
-                        rounded="50%"
-                        position="absolute"
-                        bottom="10%"
-                        right="2%"
-                        _hover={{ bg: "#005d8e", color: "white" }}
-                        onClick={() => setIsOpen(true)}
-                    >
-                        +
-                    </Button>
-                </Box>
+          <Stack gap="10"></Stack>
 
-                <DialogModal
-                    open={isOpen}
-                    onOpenChange={({ open }) => setIsOpen(open)}
-                    title="Cadastrar Usuário"
-                    textButton="Cadastrar"
-                    footer={
-                        <Button
-                            bg="#00476F"
-                            color="white"
-                            onClick={handleSubmit}
-                            loading={loading}></Button>
-                    }
-                >
-                    <Box display="flex" flexDirection="column" gap="4">
-                        <Box>
-                            <Text mb="1">Nome</Text>
-                            <Field>
-                            <Input
-                                placeholder="Digite o nome"
-                                value={nome}
-                                onChange={(e) => setNome(e.target.value)}
-                                focusRingColor="#00476F"
-                            />
-                            </Field>
-                            
-                        </Box>
-
-                        <Box>
-                            <Text mb="1">Email</Text>
-                            <Input
-                                placeholder="Digite o email"
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                focusRingColor="#00476F"
-                            />
-                        </Box>
-
-                        <Box>
-                            <Text mb="1">Senha</Text>
-                            <Input
-                                placeholder="Digite a senha"
-                                type="password"
-                                value={senha}
-                                onChange={(e) => setSenha(e.target.value)}
-                                focusRingColor="#00476F"
-                            />
-                        </Box>
-                    </Box>
-                </DialogModal>
-            </Box>
+          <Button
+            bg="#00476f"
+            fontSize="3xl"
+            w="70px"
+            h="70px"
+            rounded="50%"
+            position="absolute"
+            bottom="10%"
+            right="2%"
+            _hover={{ bg: "#005d8e", color: "white" }}
+            onClick={() => setOpen(true)}
+          >
+            +
+          </Button>
         </Box>
-    );
+
+        <Alert.Root status="info" title="This is the alert title">
+          <Alert.Indicator />
+          <Alert.Title>Preencha todos os campos!</Alert.Title>
+        </Alert.Root>
+
+        <Alert.Root status="success" variant="solid">
+          <Alert.Indicator />
+          <Alert.Title>Usuário cadastrado com sucesso!</Alert.Title>
+        </Alert.Root>
+
+        <Alert.Root status="error">
+      <Alert.Indicator />
+      <Alert.Content>
+        <Alert.Title>Erro</Alert.Title>
+        <Alert.Description>
+        Erro ao cadastrar usuário!
+        </Alert.Description>
+      </Alert.Content>
+    </Alert.Root>
+
+        <DialogModal
+          open={open}
+          onOpenChange={({ open }) => setOpen(open)}
+          title="Cadastrar Usuário"
+          textButton="Cadastrar"
+          footer={
+            <Button
+              bg="#00476F"
+              color="white"
+              onClick={handleSubmit}
+              loading={loading}
+            >
+              Cadastrar
+            </Button>
+          }
+        >
+          <Box display="flex" flexDirection="column" gap="4">
+            <Box>
+              <Text mb="1">Nome</Text>
+              <Field>
+                <Input
+                  placeholder="Digite o nome"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                  focusRingColor="#00476F"
+                />
+              </Field>
+            </Box>
+
+            <Box>
+              <Text mb="1">Email</Text>
+              <Input
+                placeholder="Digite o email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                focusRingColor="#00476F"
+              />
+            </Box>
+
+            <Box>
+              <Text mb="1">Senha</Text>
+              <Input
+                placeholder="Digite a senha"
+                type="password"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                focusRingColor="#00476F"
+              />
+            </Box>
+          </Box>
+        </DialogModal>
+      </Box>
+    </Box>
+  );
 }
--- src/app/ui/screens/deskLines.tsx
+++ src/app/ui/screens/deskLines.tsx
@@ -1,81 +1,124 @@
-import { Box, Stack } from "@chakra-ui/react";
-import DeskLinesHeader from "../components/DeskLinesHeader";
+import { Box, Center, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { FaUndo } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
 
-export default function DeskLines(){
-    return(
-        <Box 
-            display="grid"
-            gridTemplateColumns="300px 500px 200px"
-            gridTemplateRows=""
+export default function DeskLines() {
+  // Queue data
+  const [queueData] = useState([
+    { id: "A", name: "Atendimento Geral", waiting: 20, current: 19 },
+    { id: "B", name: "Marcação de Consultas", waiting: 5, current: 4 },
+    { id: "C", name: "Urgências", waiting: 76, current: 75 },
+    { id: "D", name: "Exames", waiting: 26, current: 25 },
+    { id: "P", name: "Atendimento Prioritário", waiting: 5, current: 4 },
+  ]);
+
+  return (
+    <Box 
+        w="100%"
+        mx="auto" 
+        overflow="hidden"
+        
+    >
+      
+    
+      {/* Header */}
+      <Grid
+        templateColumns="1fr 1fr"
+        bg="white"
+        color="#00476F"
+        h="50px"
+        p={2}
+        fontWeight="bold"
+      >
+        <Center as="div">
+          <Text fontSize="lg">Em Espera</Text>
+        </Center>
+        <Center as="div">
+          <Text fontSize="lg">Em Atendimento</Text>
+        </Center>
+      </Grid>
+
+      {/* Queue Items */}
+      {queueData.map((queue) => (
+        <Grid
+          key={queue.id}
+          templateColumns="1fr 1fr 100px 100px"
+          gap={2}
+          my={2}
+          p="2"
+        >
+          {/* Waiting */}
+          <GridItem>
+            <Flex
+              bg="gray.200"
+              p={5}
+              borderRadius="md"
+              justifyContent="space-evenly"
+              alignItems="center"
+              border="1px solid"
+              borderColor="teal.600"
             >
-            <DeskLinesHeader/>
-            
-            <Stack p="5">
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-            </Stack>
+              <Text fontSize="4xl" fontWeight="bold" color="teal.700">
+                {queue.id}
+              </Text>
+              <Text fontSize="4xl" fontWeight="bold" color="teal.700">
+                {queue.waiting}
+              </Text>
+            </Flex>
+          </GridItem>
 
-            <Stack p="5">
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-                <Box
-                    bg="gray.100"
-                    w="300px"
-                    h="160px"
-                    rounded="md"
-                ></Box>
-            </Stack>
+          {/* Currently being served */}
+          <GridItem>
+            <Flex
+              bg="white"
+              p={2}
+              borderRadius="md"
+              direction="column"
+              alignItems="space-around"
+              border="1px solid"
+              borderColor="teal.600"
+            >
+              <Text fontSize="lg" fontWeight="medium" color="teal.700" textAlign="center">
+                {queue.name}
+              </Text>
+              <Flex justifyContent="space-around">
+                <Text fontSize="4xl" fontWeight="bold" color="teal.700">
+                  {queue.id}
+                </Text>
+                <Text fontSize="4xl" fontWeight="bold" color="teal.700">
+                  {queue.current}
+                </Text>
+              </Flex>
+            </Flex>
+          </GridItem>
 
-        </Box>
-    )
+          {/* Navigation buttons */}
+          <GridItem>
+            <Center
+              bg="gray.200"
+              h="100%"
+              borderRadius="md"
+              color="#00476f"
+              _hover={{ bg: "#005d8e", cursor: "pointer", color:"white" }}
+            >
+              <FaArrowRight size={30} />
+            </Center>
+          </GridItem>
+
+          <GridItem>
+            <Center
+              bg="gray.200"
+              h="100%"
+              borderRadius="md"
+              color="red.500"
+              _hover={{ bg: "red.500", cursor: "pointer", color:"white" }}
+            >
+              <FaUndo size={30} />
+            </Center>
+          </GridItem>
+        </Grid>
+      ))}
+    </Box>
+  );
 }
