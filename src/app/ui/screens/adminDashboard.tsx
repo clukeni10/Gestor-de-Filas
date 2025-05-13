96fc62b0457d3d28552118caef650d2a019b50d1
Carlos Lukeni <carloslukeni110@gmail.com>
2025-05-13 17:08:06 +0100

    idk

--- src/app/ui/screens/adminDashboard.tsx
+++ src/app/ui/screens/adminDashboard.tsx
@@ -2,11 +2,13 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import SidebarMenu from "../components/SidebarMenu";
 import { FaUser } from "react-icons/fa6";
 import { database } from "../../database/firebase"; // ajuste o caminho conforme necessário
-import {  collection, getCountFromServer } from "firebase/firestore";
+import { collection, getCountFromServer } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { RiTicket2Line } from "react-icons/ri";
 
 export default function AdminDashboard() {
   const [atendentesCount, setAtendentesCount] = useState<number | null>(null);
+  const [opcoesCount, setOpcoesCount] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchAtendentesCount = async () => {
@@ -20,9 +22,22 @@ export default function AdminDashboard() {
       }
     };
 
+    const fetchOpcoesCount = async () => {
+      try {
+        const coll = collection(database, "opções-dispenser");
+        const snapshot = await getCountFromServer(coll);
+        setOpcoesCount(snapshot.data().count);
+      } catch (error) {
+        console.error("Erro ao buscar opções:", error);
+        setOpcoesCount(0);
+      }
+    }
+
+
+    fetchOpcoesCount();
     fetchAtendentesCount();
   }, []);
-  
+
 
   return (
     <Box p="5">
@@ -47,12 +62,13 @@ export default function AdminDashboard() {
             >
               <FaUser size="100" />
               <Text fontSize="2xl" mt="5">
-                {atendentesCount === null ? "Carregando..." : `${atendentesCount} Atendentes`}
+                {atendentesCount === null ? "Carregando..." : `${atendentesCount} `}
               </Text>
+              <Text fontSize="2xl">Atendentes</Text>
             </Box>
 
             <Box
-              bg="white"
+              bg="whiteAlpha.700"
               borderRadius="md"
               p={5}
               w="250px"
@@ -60,7 +76,17 @@ export default function AdminDashboard() {
               shadow="md"
               _hover={{ bg: "gray.100", cursor: "pointer" }}
               transition="0.3s"
-            ></Box>
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <RiTicket2Line size="100" />
+              <Text fontSize="2xl" mt="5">
+                {opcoesCount === null ? "Carregando" : `${opcoesCount} `}
+                <Text fontSize="2xl">Opções no Dispensador</Text>
+              </Text>
+            </Box>
 
             <Box
               bg="white"
